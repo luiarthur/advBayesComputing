cdf6dc9b8576cc351115caa92ace09b27a1bd9a6
luiarthur <luiarthur@gmail.com>
2016-01-27 18:53:33 -0800

    yes

---
+++ hw/hw1/code/R/shrink.R
@@ -0,0 +1,47 @@
+library(doMC)
+registerDoMC(ncore <- as.numeric(system("nproc",intern=T)))
+
+mvrnorm <- function(M,S,n=nrow(S)) M + t(chol(S)) %*% rnorm(n)
+
+Sig_Maker <- function(rho,p) {
+  M <- matrix(0,p,p)
+  for (i in 1:p) {
+    for (j in 1:p) {
+      M[i,j] <- rho^{abs(i-j)}
+    }
+  }
+  M
+}
+
+n <- c(50, 500)
+#p <- c(100, 1000)
+p <- c(100)
+Sig_Makers <- list( function(p) diag(p), function(p) Sig_Maker(.1,p), function(p) Sig_Maker(.6,p) )
+betas <- list( function(p) {b <- rep(0,p); b[1:5] <- 3; b }, 
+               function(p) {b <- rep(0,p); b[1:5] <- 5; b[6:10] <- -2; b[11:15] <- .5; b },
+               function(p) {b <- rep(1,p); b })
+
+numdat <- length(n) * length(p) * length(Sig_Makers) * length(betas)
+counter <- 0
+dat <- as.list(1:numdat)
+
+
+for (nn in n) {
+  for (pp in p) {
+    for (ss in Sig_Makers) {
+      for (bb in betas) {
+        counter <- counter + 1
+        s <- ss(pp)
+        x <- foreach(i=1:nn,.combine=rbind) %dopar% c(mvrnorm(0,ss(pp)))
+        y <- x %*% bb(pp) + rnorm(nn)
+
+        #dat[[counter]] <- list("y"=y, "x"=x, "S"=s, "n"=nn, "p"=pp)
+
+        cat("\r n: ", nn, ";  p: ", pp)
+      }
+    }
+  }
+}
+
+
+
