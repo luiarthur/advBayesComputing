5479e6bc0ee503d3056b7338bbe56663ca02995f
luiarthur <luiarthur@gmail.com>
2016-02-22 14:03:07 -0800

    yes

--- hw/hw2/code/C++/gp.cpp
+++ hw/hw2/code/C++/gp.cpp
@@ -4,13 +4,17 @@
 //[[Rcpp::export]]
 
 //[[Rcpp::export]]
-mat gp(vec y, mat x, mat V, int B, int burn, bool printProg) {
+mat gp(vec y, mat x, mat R, int B, int burn, bool printProg) {
   int n = y.size();
   int p = x.n_cols; 
   mat xt = x.t();
   mat S = (xt * x + V.i()).i();
   mat SX = S*xt;
   mat beta = zeros<mat>(B+burn,p);
+  mat Ri = R.i();
+  mat In = eye<mat>(n,n);
+
+  // 1/s2 * In - t2/(s2*s2) ()
 
   for (int b=1; b<B+burn; b++) {
     beta.row(b) = reshape(mvrnorm(SX * z, S),1,p);
