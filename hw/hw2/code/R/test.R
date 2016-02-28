bb8afd222a5b40636d2219099dd90aebb6ddfba1
luiarthur <luiarthur@gmail.com>
2016-02-28 10:56:05 -0800

    yes

--- hw/hw2/code/R/test.R
+++ hw/hw2/code/R/test.R
@@ -1,6 +1,6 @@
 set.seed(268)
 system("mkdir -p output")
-source("../../../../R_Functions/plotPost.R",chdir=T)
+source("plotmap.R")
 library(spBayes)
 
 spLM
@@ -22,12 +22,23 @@ D <- as.matrix(dist(s))
 
 # y | ... ~ N(0,s^2 + K)
 #priors <- c(2,.5,  4.9,5.1,  2,2) #s2, phi, tau
-priors <- list("sigma.sq.IG"=c(2,1/.5),
+priors <- list("sigma.sq.IG"=c(2,.5),
                "phi.Unif"=c(0.1,5),
-               "tau.sq.IG"=c(2,1/2))
+               "tau.sq.IG"=c(2,2))
 starting <- list("phi"=1,"sigma.sq"=1,"tau.sq"=1)
 tuning <- list("phi"=.01,"sigma.sq"=.01,"tau.sq"=.01)
-out <- spLM(y~rep(0,n),coords=x,knots=s,cov.model="exponential",n.samples=300,priors=priors,starting=starting,tuning=tuning,verbose=T,modified.pp=F)
-#system.time( out <- gp(y, x, s, C, D, cand_S=.01*diag(3), init=rep(0,3), priors=priors, B=3000, burn=15000, printProg=T) )
+out <- spLM(y~rep(1,n)-1,coords=x,knots=s,cov.model="exponential",n.samples=10000,priors=priors,starting=starting,tuning=tuning,verbose=T,modified.pp=F)
 
+out$p.theta.samples
+m1 <- spRecover(out,start=8000,verbose=TRUE)
+preds <- m1$p.w.recover.samples
 
+col.map <- colorRampPalette(c('darkred','orange','yellow'),bias=2)(length(mu))
+col.diff <- colorRampPalette(c('darkblue','white','darkred'))(length(mu))
+
+par(mfrow=c(2,2))
+  plotmap(f(m1$coords),m1$coords, bks=c(0,1),xlim=c(-3,3),ylim=c(-3,3),col.map=col.map,ylab="x2",xlab="x1"); abline(v=c(.5),col="grey")
+  plotmap(apply(preds,1,mean),m1$coords, bks=c(0,1),xlim=c(-3,3),ylim=c(-3,3),col.map=col.map,ylab="x2",xlab="x1"); abline(v=c(.5),col="grey")
+  plotmap(f(m1$coords)-apply(preds,1,mean),m1$coords, bks=c(-1,1)*.2,xlim=c(-3,3),ylim=c(-3,3),col.map=col.diff,ylab="x2",xlab="x1"); abline(v=c(.5),col="grey")
+  plotmap(apply(preds,1,sd),m1$coords, bks=c(0,1),xlim=c(-3,3),ylim=c(-3,3),col.map=col.map,ylab="x2",xlab="x1"); abline(v=c(.5),col="grey")
+par(mfrow=c(1,1))
