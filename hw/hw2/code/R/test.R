c10a33f56fc51635c7035ec0fe6a6e71f80ec9fc
luiarthur <luiarthur@gmail.com>
2016-02-28 20:54:26 -0800

    yes

--- hw/hw2/code/R/test.R
+++ hw/hw2/code/R/test.R
@@ -23,15 +23,17 @@ D <- as.matrix(dist(s))
 
 # y | ... ~ N(0,s^2 + K)
 #priors <- c(2,.5,  4.9,5.1,  2,2) #s2, phi, tau
-priors <- list("sigma.sq.IG"=c(2,2), # the scaling for the covariance matrix
-               "phi.Unif"=c(0.1,5),
-               "tau.sq.IG"=c(2,.5)) # the variance between observations
+priors <- list("sigma.sq.IG"=c(2,2), # the scaling for the covariance matrix. 
+               "phi.Unif"=c(0.1,5),  # decay parameter
+               "tau.sq.IG"=c(2,.5),  # the variance between observations. Should be .5
+               "beta.norm"=list(0,.00001))
 starting <- list("phi"=1,"sigma.sq"=1,"tau.sq"=1)
 tuning <- list("phi"=.01,"sigma.sq"=.01,"tau.sq"=.01)
 out <- spLM(y~1,coords=x,knots=s,cov.model="exponential",n.samples=12000,priors=priors,starting=starting,tuning=tuning,verbose=T,modified.pp=F)
 
-out$p.theta.samples
-m1 <- spRecover(out,start=10000+1,verbose=TRUE)
+
+plot.posts(as.matrix(out$p.theta.samples))
+m1 <- spRecover(out,start=10001,verbose=TRUE)
 preds <- m1$p.w.recover.samples
 
 col.map <- colorRampPalette(c('darkred','orange','yellow'),bias=2)(length(mu))
@@ -43,3 +45,4 @@ par(mfrow=c(2,2))
   plotmap(f(m1$coords)-apply(preds,1,mean),m1$coords, bks=c(-1,1)*.2,xlim=c(-3,3),ylim=c(-3,3),col.map=col.diff,ylab="x2",xlab="x1"); abline(v=c(.5),col="grey")
   plotmap(apply(preds,1,sd),m1$coords, bks=c(0,1),xlim=c(-3,3),ylim=c(-3,3),col.map=col.map,ylab="x2",xlab="x1"); abline(v=c(.5),col="grey")
 par(mfrow=c(1,1))
+
