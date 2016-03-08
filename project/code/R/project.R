f6ddc068c1cb55dd0ba1cf7311fefdf00a515749
luiarthur <luiarthur@gmail.com>
2016-03-08 01:30:11 -0800

    yes

--- project/code/R/project.R
+++ project/code/R/project.R
@@ -19,10 +19,6 @@ priors <- c(5,5,  .1,5,  .1,.1) #s2, phi, tau
 out <- gp(y=dat$y, x=dat$x, D=dat$D, cand_S=diag(dat$p)*.1,
           init=rep(1,3), priors=priors, B=2000, burn=4000, printProg=TRUE)
 colnames(out$param) <- c("s2","phi","tau")
-#plot.posts(out$param,cex.l=1.3,cex.a=1,names=colnames(out$param))
-#plot(ts(out$param))
-#apply(out$param,2,summary)
-
 
 ### p = 500
 f3.2 <- function(X) apply(as.matrix(X),1, function(x) 10*x[1] + 15*sin(x[2]) + 10*x[3]^2)
@@ -30,13 +26,21 @@ dat2 <- sim_dat(f3.2,p=500,n=100)
 priors <- c(2,1,    .1,5,    2,5,    1,1) #s2, phi, tau, d_vec
 cat("sourcing gp_gdp.cpp...\n"); sourceCpp("../C++/gp_gdp.cpp")
 out2 <- gp_gdp(y=dat2$y, X=dat2$x, cand_S=diag(3+dat2$p)*1e-9,
-               init=rep(0,3+dat2$p), priors=priors, B=10, burn=0, printProg=TRUE)
-               #init=rep(0,3+dat2$p), priors=priors, B=2000, burn=100000, printProg=TRUE)
+               init=rep(0,3+dat2$p), priors=priors, B=2, burn=1, printProg=TRUE)
 save(out2,file="output/out2.RData")
 load("output/out2.RData")
-#plot(ts(out2$param))
 
 
-#plot.posts(out2$param[,1:3],cex.l=1.3,cex.a=1)
-#plot( apply(out2$param[,-c(1:3)],2,mean) )
-#apply(out2$param,2,summary)
+#Plots: #####################################################3
+
+### p=3
+plot.posts(out$param,cex.l=1.3,cex.a=1,names=colnames(out$param))
+plot(ts(out$param))
+apply(out$param,2,summary)
+
+
+### p=500
+plot(ts(out2$param))
+plot.posts(out2$param[,1:3],cex.l=1.3,cex.a=1)
+plot( apply(out2$param[,-c(1:3)],2,mean) )
+apply(out2$param,2,summary)
