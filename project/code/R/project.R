647c4de05e642a88b14497cf2a8f0ff60c81aba5
luiarthur <luiarthur@gmail.com>
2016-03-08 08:54:40 -0800

    yes

--- project/code/R/project.R
+++ project/code/R/project.R
@@ -22,11 +22,11 @@ colnames(out$param) <- c("s2","phi","tau")
 
 ### p = 500
 f3.2 <- function(X) apply(as.matrix(X),1, function(x) 10*x[1] + 15*sin(x[2]) + 10*x[3]^2)
-dat2 <- sim_dat(f3.2,p=500,n=100)
+dat2 <- sim_dat(f3.2,p=7,n=100)
 priors <- c(2,1,    .1,5,    2,5,    1,1) #s2, phi, tau, d_vec
 cat("sourcing gp_gdp.cpp...\n"); sourceCpp("../C++/gp_gdp.cpp")
 out2 <- gp_gdp(y=dat2$y, X=dat2$x, cand_S=diag(3+dat2$p)*1e-9,
-               init=rep(0,3+dat2$p), priors=priors, B=2000, burn=100000, printProg=TRUE)
+               init=rep(0,3+dat2$p), priors=priors, B=2000, burn=1000, printProg=TRUE)
 save(out2,file="output/out2.RData")
 load("output/out2.RData")
 
@@ -40,6 +40,7 @@ apply(out$param,2,summary)
 
 
 ### p=500
+out2$acc_rate
 plot(ts(out2$param))
 plot.posts(out2$param[,1:3],cex.l=1.3,cex.a=1)
 plot( apply(out2$param[,-c(1:3)],2,mean) )
